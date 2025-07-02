98ee89585b888b4001cf485154e0fe904b180be3
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2025-07-02 12:34:42 +0530

    Add category page details handler to fetch courses by category in Category controller

--- SERVER/controllers/category.js
+++ SERVER/controllers/category.js
@@ -39,11 +39,8 @@ exports.createCategory = async(req,res)=>{
     }
 }
 
-
-
-
 // get all the tags
-exports.showAllTags = async(req,res)=>{
+exports.showAllCategories= async(req,res)=>{
     try {
         // get all category from DB
         const allCategories = await Category.find({}, { name: true, description: true });
@@ -63,4 +60,56 @@ exports.showAllTags = async(req,res)=>{
         })
     }
 }
+
+
+
+
+
+//catagories Page Details
+exports.categoryPageDetails = async(req,res)=>{
+  try{
+
+    //get CategoriesId
+      const {categoryId}  = req.body;
+    //get courses for specified categoryId
+      const selectedCategory  = await Category.findById(categoryId)
+                        .populate("courses")
+                        .exec();
+    //validation    
+    if(!selectedCategory){
+        return res.status(404).json({
+            success: false,
+            message: 'Category not found',
+        })
+    }
+    //get courses foe different categories
+     const differentCategories = await Category.find({
+        _id: {$ne : categoryId},   //eq->equal ,ne->!eq
+                })
+                .populate("courses")
+                .exec();
+
+    //get top 10 selling courses  -< Todo
+
+
+    //return response
+    return res.status(200).json({
+        success: true,
+        data: {selectedCategory,differentCategories}, 
+    })
+
+
+  }catch(error){
+      console.log(error);
+      return res.status(500).json({
+        success:false,
+        message:error.message,
+      })
+
+  }
+}
+
+
+
+
 
