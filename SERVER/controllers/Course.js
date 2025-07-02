4f60d4b36cc78962b7baf084f2c6a048feb24c62
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2025-07-02 12:35:52 +0530

    Add getCourseDetails handler to fetch detailed course information in Course controller

--- SERVER/controllers/Course.js
+++ SERVER/controllers/Course.js
@@ -98,9 +98,6 @@ exports.createCourse = async(req,res)=>{
     })
 }
 }
-
-
-
 //getAllCourse handler function
  exports.getAllCourses = async (req, res) => {
     try {
@@ -132,4 +129,63 @@ exports.createCourse = async(req,res)=>{
         })
     }
 }
+
+//getCourseDetails
+exports.getCourseDetails = async(req,res)=>{
+    try{
+        //get the course id
+        const {courseId} = req.body;
+        // find  the course details
+        const courseDetails = await Course.find(
+            {_id:courseId})
+            .populate(
+                {
+                    path:"instructor",
+                    populate:{
+                        path:"additionalDetails",
+                    }
+                }
+            )
+          .populate("category")
+          .populate("ratingAndreviews")
+          .populate({
+            path:"courseContent",
+            populate:{
+                path:"subSection",
+            },
+          })
+          .exec();
+
+
+          //validation
+          if(!courseDetails){
+            return res.status(400).json({
+                success:false,
+                message:`Could not find the course with ${courseId}`,
+            })  
+        }
+        
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:"Course details fetched successfully",
+            data:courseDetails
+        })
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        })
+
+    }
+}
+
+
+
+
+
+
+
 
