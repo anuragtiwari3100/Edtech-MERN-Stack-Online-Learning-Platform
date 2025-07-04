aaaaae3e49ce0b84b0a89c3ffd0516fa4b3e25c3
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2025-07-04 11:51:43 +0530

    Add utility function to convert seconds to duration format

--- SERVER/utils/imageUploader.js
+++ SERVER/utils/imageUploader.js
@@ -15,3 +15,19 @@ exports.uploadImageToCloudinary = async (file,folder,height,quality)=>{
 
    return await cloudinary.uploader.upload(file.tempFilePath.options);
 }
+
+
+// Function to delete a resource by public ID
+exports.deleteResourceFromCloudinary = async (url) => {
+    if (!url) return;
+
+    try {
+        const result = await cloudinary.uploader.destroy(url);
+        console.log(`Deleted resource with public ID: ${url}`);
+        console.log('Delete Resourse result = ', result)
+        return result;
+    } catch (error) {
+        console.error(`Error deleting resource with public ID ${url}:`, error);
+        throw error;
+    }
+};
