08a1f5eb6cddfe3cafff7f233a56fd622cbd3134
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2025-07-13 12:23:21 +0530

    modularize Home page with reusable component - Created reusable components: Footer, Img, CourseCard, ExploreMore,   InstructorSection, LearningLanguageSection, TimelineSection - Integrated them into Home.jsx - Improved code organization and reusability

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,6 +5,12 @@ import  HighlightText  from "../components/core/HomePage/HighlightText"
 import CTAButton  from "../components/core/HomePage/Button" 
 import  CodeBlocks  from "../components/core/HomePage/CodeBlocks"
 import Banner from  "../assets/Images/banner.mp4"
+import TimelineSection from '../components/core/HomePage/TimelineSection';
+import  LearningLanguageSection  from "../components/core/HomePage/LearningLanguageSection"
+import  InstructorSection  from "../components/core/HomePage/InstructorSection"
+import Footer from './../components/common/Footer';
+import ExploreMore from '../components/core/HomePage/ExploreMore'
+
 
 const Home = () => {
   return (
@@ -125,11 +131,59 @@ const Home = () => {
                             backgroundGradient={"code-block1-grad"}
          />
         </div>
+
+         <ExploreMore />
     </div>
      
 
+        {/*Section 2  */}
+                <div className='bg-pure-greys-5 text-richblack-700 '>
+                    <div className='homepage_bg h-[310px]'>
+                        <div className='w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-5 mx-auto'>
+                            <div className='h-[150px]'></div>
+                            <div className='flex flex-row gap-7 text-white '>
+                                <CTAButton active={true} linkto={"/signup"}>
+                                    <div className='flex items-center gap-3' >
+                                        Explore Full Catalog
+                                        <FaArrowRight />
+                                    </div>
+                                </CTAButton>
+                                <CTAButton active={false} linkto={"/signup"}>
+                                    <div>
+                                        Learn more
+                                    </div>
+                                </CTAButton>
+                            </div>
+                        </div>
+                    </div>
 
+              <div className='mx-auto w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-7'>
+              
+                   <div className='flex flex-col lg:flex-row gap-5 mb-10 mt-[95px]'>
+                     <div className='text-3xl lg:text-4xl font-semibold w-full lg:w-[45%]'>
+                       Build the skills that
+                        <HighlightText text={"top employers are looking for today"}/>
+                     </div>
 
+                     <div className='flex flex-col gap-10 w-full lg:w-[40%] items-start' >
+                        <div className='text-[16px]'>
+                                     The modern StudyNotion is the dictates its own terms. Today, to be a competitive specialist requires more than professional skills.
+                        </div>
+                       <CTAButton active={true} linkto={"/signup"}>
+                          <div>
+                           Learn more
+                          </div>
+                       </CTAButton>                         
+                     </div>
+
+                   </div>
+                       {/* Leadership */}
+               <TimelineSection/>
+
+               <LearningLanguageSection/>
+              </div>
+
+           
 
 
         {/* Section 3 */}
@@ -137,6 +191,16 @@ const Home = () => {
 
 
         {/* Footer */}
+     </div>
+    
+       {/* Section 3 */}
+       <div className='mt-14 w-11/12 mx-auto max-w-maxContent flex-col items-center justify-between gap-8 first-letter bg-richblack-900 text-white'>
+                    <InstructorSection />
+
+       </div>
+      
+       {/*Footer */}
+       <Footer />
      </div>
   )
 }
