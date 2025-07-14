ec0c921de16b15ec571a3c1a2fde2ef4ae931129
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2025-07-14 15:45:01 +0530

    nitializes token from localStorage and provides reducer to update it

---
+++ Frontend/src/slice/authSlice.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { createSlice } from '@reduxjs/toolkit'
+  
+const initialState = {
+    token: localStorage.getItem("token") || null,
+
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState:initialState,
+  reducers:{
+    setToken(state,value){
+      state.token = value.payload;
+    },
+  },
+})
+  
+
+export  const {setToken} = authSlice.actions;
+export default authSlice.reducer;
